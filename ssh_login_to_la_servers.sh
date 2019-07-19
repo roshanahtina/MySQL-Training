187ac6ad4c7494c2ff2180fe594a1ca97e63b514
Roshan Munavar <roshan.ahtina@gmail.com>
2019-07-19 20:30:26 +0530

    Update ssh_login_to_la_servers.sh

--- ssh_login_to_la_servers.sh
+++ ssh_login_to_la_servers.sh
@@ -5,5 +5,5 @@ login_user=ansible
 for HOST in $(cat labservers.txt)
    do
      echo "####################### Logining in LAB Servers $HOST #######################"
-	 sudo -t ssh $login_user@$HOST 'uname -a'
+	 sudo -S ssh $login_user@$HOST 'uname -a'
    done
