462f226a43a08afdc55f8d3a87c8d3c0cbefe0ad
roshanahtina <roshan.ahtina@gmail.com>
2019-07-19 14:08:32 +0000

    ssh_login_to_la_servers.sh

---
+++ ssh_login_to_la_servers.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#ssh_file=/home/cloud_user/MySQL-Training/labservers.txt
+
+for HOST in $(cat /home/cloud_user/MySQL-Training/labservers.txt)
+   do
+     echo "####################### Logining in LAB Servers $HOST #######################"
+	 ssh #HOST 'uname -a'
+   done
+exit()
