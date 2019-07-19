9c643989c10378a380a1925e2da11796562563c2
Roshan Munavar <roshan.ahtina@gmail.com>
2019-07-19 20:39:52 +0530

    Update ssh_login_to_la_servers.sh

--- ssh_login_to_la_servers.sh
+++ ssh_login_to_la_servers.sh
@@ -4,6 +4,6 @@ login_user=ansible
 
 for HOST in $(cat labservers.txt)
    do
-     echo "####################### Logining in LAB Servers $HOST #######################"
+     echo -e "####################### Logining in LAB Servers $HOST #######################\n\n"
 	 ssh $login_user@$HOST 'uname -a'
    done
