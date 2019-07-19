9788e24578920c7808cb417dcd739a4ec48fac0d
Roshan Munavar <roshan.ahtina@gmail.com>
2019-07-19 19:55:13 +0530

    Update ssh_login_to_la_servers.sh

--- ssh_login_to_la_servers.sh
+++ ssh_login_to_la_servers.sh
@@ -4,6 +4,6 @@
 for HOST in $(cat labservers.txt)
    do
      echo "####################### Logining in LAB Servers $HOST #######################"
-	 ssh #HOST 'uname -a'
+	 ssh #HOST `uname -a`
    done
 exit()
