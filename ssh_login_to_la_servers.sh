9d68490e4d14f39241c3c7a676758a6cdeff45b0
Roshan Munavar <roshan.ahtina@gmail.com>
2019-07-19 20:23:05 +0530

    Update ssh_login_to_la_servers.sh

--- ssh_login_to_la_servers.sh
+++ ssh_login_to_la_servers.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#login_user=ansible
+login_user=ansible
 #ssh_file=/home/cloud_user/MySQL-Training/labservers.txt
 
 for HOST in $(cat labservers.txt)
    do
      echo "####################### Logining in LAB Servers $HOST #######################"
-	 ssh $HOST `uname -a`
+	 ssh $login_user@$HOST `uname -a`
    done
