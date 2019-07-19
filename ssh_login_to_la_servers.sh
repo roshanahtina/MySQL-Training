a5badd50f318ca1aa86e8362eccecc0c0669d245
Roshan Munavar <roshan.ahtina@gmail.com>
2019-07-19 19:58:02 +0530

    Update ssh_login_to_la_servers.sh

--- ssh_login_to_la_servers.sh
+++ ssh_login_to_la_servers.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+#login_user=cloud_user
 #ssh_file=/home/cloud_user/MySQL-Training/labservers.txt
 
 for HOST in $(cat labservers.txt)
    do
      echo "####################### Logining in LAB Servers $HOST #######################"
-	 ssh #HOST `uname -a`
+	 ssh #$HOST `uname -a`
    done
 exit()
