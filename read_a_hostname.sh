7d6e169b529b78f658487f8264d78b9930e237fd
Roshan Munavar <roshan.ahtina@gmail.com>
2019-07-21 21:16:02 +0530

    Update read_a_hostname.sh

--- read_a_hostname.sh
+++ read_a_hostname.sh
@@ -11,3 +11,5 @@ ping -c 2 $labserver
 echo -e "######### SSH Command executes ##########"
 ssh $login_user@$labserver 'uname -r'
 done < $labservers
+
+while read HOST ; do echo "" | ssh $HOST "uname -a" ; done < servers.txt
