72c99615a2aa9a95a198ebf8580a62fe0a6b52ff
Roshan Munavar <roshan.ahtina@gmail.com>
2019-07-22 08:11:25 +0530

    Update getthehostname.py

--- MySqlDir/getthehostname.py
+++ MySqlDir/getthehostname.py
@@ -2,7 +2,7 @@ import socket
 
 def get_the_hostname():
      host_name = socket.gethostname()
-     ip_add = socket.gethostbyname()
+     ip_add = socket.gethostbyname(host_name)
      print ("Hostname : ", host_name)
      print ("IP_ADD : ", ip_add)
 
